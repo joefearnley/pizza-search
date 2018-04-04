8c5464b9232f995ab02bac47897f47811b954206
Joe Fearnley <joe.fearnley@gmail.com>
2018-04-04 18:10:18 +0000

    Removing mock http calls

--- tests/search.test.js
+++ tests/search.test.js
@@ -1,27 +1,17 @@
 const baseUrl = `http://localhost:${process.env.PORT}`;
 const request = require('supertest')(baseUrl);
-// const nock = require('nock');
 const  server = require('../server');
 const city = 'Muskegon,MI';
-const fs = require('fs');
-const fakeReponse = require('./response');
 
 describe('GET /search', () => {
-  test('It returns a 422 when no city is provided', async () => {
-    let response = await request.get(`/search`);
-    expect(response.status).toBe(422);
-  });
-});
-
-describe('GET /search?city=', () => {
-  // beforeEach(() => {
-  //   nock(baseUrl)
-  //     .get(`/search?city=${city}`)
-  //     .reply(200, fakeReponse);
-  // });
 
   afterAll(() => {
     server.close();
+  });
+
+  test('It returns a 422 when no city is provided', async () => {
+    let response = await request.get(`/search`);
+    expect(response.status).toBe(422);
   });
 
   test('It returns a 200 when location is provided', async () => {
