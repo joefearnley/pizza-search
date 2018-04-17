6e12a46d75cda9342d94d5188e9e508b186bfec5
Joe Fearnley <joe.fearnley@gmail.com>
2018-04-17 19:36:20 +0000

    Adding tests for lat/lon

--- tests/search.test.js
+++ tests/search.test.js
@@ -1,9 +1,11 @@
-const baseUrl = `http://localhost:${process.env.PORT}`;
+const baseUrl = `http://localhost:${(process.env.PORT || 5000)}`;
 const request = require('supertest')(baseUrl);
 const  server = require('../server');
 const nock = require('nock');
 const fakeResponse = require('./response');
 const city = 'Muskegon,MI';
+const latitude = 43.224194;
+const longitude = -86.235809;
 
 describe('GET /search', () => {
 
@@ -21,13 +23,22 @@ describe('GET /search', () => {
     expect(response.status).toBe(422);
   });
 
-
-  test('It returns a 200 when location is provided', async () => {
+  test('It returns a 200 when city is provided', async () => {
     let response = await request.get(`/search?city=${city}`);
     expect(response.status).toBe(200);
   });
 
-  test('It returns a results when city is provided', async () => {
+  test('It returns a 422 when longitude is missing', async () => {
+    const response = await request.get(`/search?latitude=${latitude}`)
+    expect(response.status).toBe(422);
+  });
+
+  test('It returns a 422 when latitude is missing', async () => {
+    const response = await request.get(`/search?longitude=${longitude}`)
+    expect(response.status).toBe(422);
+  });
+
+  test('It returns results when city is provided', async () => {
     nock(baseUrl)
       .get(`/search?city=${city}`)
       .reply(200, fakeResponse);
@@ -41,10 +52,7 @@ describe('GET /search', () => {
     expect(firstLocation.name).toBe('Rebel Pies');
   });
 
-  test('It returns a results when latitude and longitude are provided', async () => {
-    const latitude = 43.224194;
-    const longitude = -86.235809;
-
+  test('It returns results when latitude and longitude are provided', async () => {
     nock(baseUrl)
       .get(`/search?latitude=${latitude}&longitude=${longitude}`)
       .reply(200, fakeResponse);
